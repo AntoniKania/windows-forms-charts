52f51dcf06b628cd98ee6029119b26b655c1c21a
Antoni Kania <antek.kania@wp.pl>
2022-03-12 18:55:02 +0100

    komp

--- charts/Form2.Designer.cs
+++ charts/Form2.Designer.cs
@@ -28,47 +28,189 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series13 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series14 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Title title7 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title3 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zapiszJakoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.widokToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pokażSerieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.HorizontalMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.HorizontalMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.HorizontalMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.VerticalMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.VerticalMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.VerticalMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
             this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.menuStrip2.SuspendLayout();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea7.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea7);
-            legend7.Name = "Legend1";
-            this.chart1.Legends.Add(legend7);
-            this.chart1.Location = new System.Drawing.Point(12, 12);
+            chartArea3.AxisX.MajorGrid.Interval = 0D;
+            chartArea3.AxisX.MajorGrid.IntervalOffset = 0D;
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend3.Alignment = System.Drawing.StringAlignment.Far;
+            legend3.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
+            legend3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            legend3.IsTextAutoFit = false;
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(0, 27);
             this.chart1.Name = "chart1";
-            series13.ChartArea = "ChartArea1";
-            series13.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series13.Legend = "Legend1";
-            series13.Name = "Odczyt Temperatur";
-            series14.ChartArea = "ChartArea1";
-            series14.Legend = "Legend1";
-            series14.Name = "Różnica Temperatur";
-            this.chart1.Series.Add(series13);
-            this.chart1.Series.Add(series14);
-            this.chart1.Size = new System.Drawing.Size(776, 426);
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.Legend = "Legend1";
+            series5.Name = "Odczyt Temperatur";
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Różnica Temperatur";
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(982, 530);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "title";
-            title7.Name = "Wykres Pomiarów";
-            title7.Text = "Wykresy Pomiarów";
-            this.chart1.Titles.Add(title7);
+            title3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            title3.Name = "Wykres Pomiarów";
+            title3.Text = "Wykresy Pomiarów";
+            this.chart1.Titles.Add(title3);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.AutoSize = false;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem,
+            this.widokToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(982, 27);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zapiszJakoToolStripMenuItem});
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 23);
+            this.plikToolStripMenuItem.Text = "Plik";
+            // 
+            // zapiszJakoToolStripMenuItem
+            // 
+            this.zapiszJakoToolStripMenuItem.Name = "zapiszJakoToolStripMenuItem";
+            this.zapiszJakoToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.zapiszJakoToolStripMenuItem.Text = "Zapisz jako...";
+            // 
+            // widokToolStripMenuItem
+            // 
+            this.widokToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pokażSerieToolStripMenuItem,
+            this.HorizontalMenuItem3});
+            this.widokToolStripMenuItem.Name = "widokToolStripMenuItem";
+            this.widokToolStripMenuItem.Size = new System.Drawing.Size(53, 23);
+            this.widokToolStripMenuItem.Text = "Widok";
+            // 
+            // pokażSerieToolStripMenuItem
+            // 
+            this.pokażSerieToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.HorizontalMenuItem1,
+            this.HorizontalMenuItem2});
+            this.pokażSerieToolStripMenuItem.Name = "pokażSerieToolStripMenuItem";
+            this.pokażSerieToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.pokażSerieToolStripMenuItem.Text = "Ukryj Serie";
+            // 
+            // HorizontalMenuItem1
+            // 
+            this.HorizontalMenuItem1.Name = "HorizontalMenuItem1";
+            this.HorizontalMenuItem1.Size = new System.Drawing.Size(173, 22);
+            this.HorizontalMenuItem1.Text = "Odczyt temperatur";
+            this.HorizontalMenuItem1.Click += new System.EventHandler(this.HorizontalMenuItem1_Click);
+            // 
+            // HorizontalMenuItem2
+            // 
+            this.HorizontalMenuItem2.Name = "HorizontalMenuItem2";
+            this.HorizontalMenuItem2.Size = new System.Drawing.Size(173, 22);
+            this.HorizontalMenuItem2.Text = "Różnica temeratur";
+            this.HorizontalMenuItem2.Click += new System.EventHandler(this.HorizontalMenuItem2_Click);
+            // 
+            // HorizontalMenuItem3
+            // 
+            this.HorizontalMenuItem3.Name = "HorizontalMenuItem3";
+            this.HorizontalMenuItem3.Size = new System.Drawing.Size(149, 22);
+            this.HorizontalMenuItem3.Text = "Ukryj Legendę";
+            this.HorizontalMenuItem3.Click += new System.EventHandler(this.HorizontalMenuItem3_Click);
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.menuStrip2.GripMargin = new System.Windows.Forms.Padding(5, 2, 5, 2);
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.VerticalMenuItem1,
+            this.VerticalMenuItem2,
+            this.VerticalMenuItem3});
+            this.menuStrip2.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.Table;
+            this.menuStrip2.Location = new System.Drawing.Point(982, 0);
+            this.menuStrip2.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Padding = new System.Windows.Forms.Padding(10, 2, 0, 2);
+            this.menuStrip2.Size = new System.Drawing.Size(203, 557);
+            this.menuStrip2.Stretch = false;
+            this.menuStrip2.TabIndex = 5;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // VerticalMenuItem1
+            // 
+            this.VerticalMenuItem1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.VerticalMenuItem1.AutoSize = false;
+            this.VerticalMenuItem1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.VerticalMenuItem1.Margin = new System.Windows.Forms.Padding(0, 100, 0, 20);
+            this.VerticalMenuItem1.MergeIndex = 10;
+            this.VerticalMenuItem1.Name = "VerticalMenuItem1";
+            this.VerticalMenuItem1.Padding = new System.Windows.Forms.Padding(0, 4, 0, 4);
+            this.VerticalMenuItem1.Size = new System.Drawing.Size(183, 39);
+            this.VerticalMenuItem1.Text = "Ukryj Serię: Odczyt temperatur";
+            this.VerticalMenuItem1.Click += new System.EventHandler(this.VerticalMenuItem1_Click);
+            // 
+            // VerticalMenuItem2
+            // 
+            this.VerticalMenuItem2.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.VerticalMenuItem2.AutoSize = false;
+            this.VerticalMenuItem2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.VerticalMenuItem2.Margin = new System.Windows.Forms.Padding(0, 0, 10, 20);
+            this.VerticalMenuItem2.MergeIndex = 10;
+            this.VerticalMenuItem2.Name = "VerticalMenuItem2";
+            this.VerticalMenuItem2.Size = new System.Drawing.Size(183, 39);
+            this.VerticalMenuItem2.Text = "Ukryj Serię: Różnica temperatur";
+            this.VerticalMenuItem2.Click += new System.EventHandler(this.VerticalMenuItem2_Click);
+            // 
+            // VerticalMenuItem3
+            // 
+            this.VerticalMenuItem3.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.VerticalMenuItem3.AutoSize = false;
+            this.VerticalMenuItem3.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.VerticalMenuItem3.Margin = new System.Windows.Forms.Padding(0, 0, 10, 0);
+            this.VerticalMenuItem3.MergeIndex = 10;
+            this.VerticalMenuItem3.Name = "VerticalMenuItem3";
+            this.VerticalMenuItem3.Size = new System.Drawing.Size(183, 39);
+            this.VerticalMenuItem3.Text = "Ukryj Legendę";
+            this.VerticalMenuItem3.Click += new System.EventHandler(this.VerticalMenuItem3_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(621, 121);
+            this.button1.Location = new System.Drawing.Point(1045, 395);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(98, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Ukryj Legendę";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "+";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -76,20 +218,40 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1185, 557);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.chart1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Wykres";
             this.Text = "Wykres";
             this.Load += new System.EventHandler(this.Wykres_Load);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem widokToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zapiszJakoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pokażSerieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem HorizontalMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem HorizontalMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem HorizontalMenuItem3;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem VerticalMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem VerticalMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem VerticalMenuItem3;
         private System.Windows.Forms.Button button1;
     }
 }
--- charts/Form2.cs
+++ charts/Form2.cs
@@ -16,8 +16,10 @@ namespace charts
     public partial class Wykres : Form
     {
         public static Legend legend;
+        public static Series s1, s2;
         List<double> samples = new List<double>();
         public static string title;
+        public static ChartArea chartArea;
         public Wykres(List<double> data, string text)
         {
             samples = data;
@@ -32,10 +34,15 @@ namespace charts
         private void Wykres_Load(object sender, EventArgs e)
         {
             legend = chart1.Legends["Legend1"];
-            Series s1, s2;
+            VerticalMenuItem3.Text = "Ukryj Legendę";
+            VerticalMenuItem1.Text = "Ukryj Serię: \nOdczyt temperatur";
+            VerticalMenuItem2.Text = "Ukryj Serię: \nRóżnica temperatur";
             s1 = chart1.Series["Odczyt Temperatur"];
             s2 = chart1.Series["Różnica Temperatur"];
-            s1.LabelToolTip = title;
+            Title mainTitle = chart1.Titles["Wykres Pomiarów"];
+            mainTitle.Text = title;
+            chartArea = chart1.ChartAreas["ChartArea1"];
+
             for (int x = 0; x < samples.Count; x++)
                 s1.Points.AddXY(x + 1, samples[x]);
             for(int x2 = 1; x2 < samples.Count; x2++)
@@ -43,22 +50,81 @@ namespace charts
 
         }
 
+
+        private void VerticalMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (s1.Enabled == true)
+            {
+                s1.Enabled = false;
+                VerticalMenuItem1.Text = "Pokaż Serię: \nOdczyt temperatur";
+                HorizontalMenuItem1.Checked = true;
+            }
+
+            else
+            {
+                s1.Enabled = true;
+                VerticalMenuItem1.Text = "Ukryj Serię: \nOdczyt temperatur";
+                HorizontalMenuItem1.Checked = false;
+            }
+        }
+        private void VerticalMenuItem2_Click(object sender, EventArgs e)
+        {
+            if (s2.Enabled == true)
+            {
+                s2.Enabled = false;
+                VerticalMenuItem2.Text = "Pokaż Serię: \nRóżnica temperatur";
+                HorizontalMenuItem2.Checked = true;
+            }
+
+            else
+            {
+                s2.Enabled = true;
+                VerticalMenuItem2.Text = "Ukryj Serię: \nRóżnica temperatur";
+                HorizontalMenuItem2.Checked = false;
+            }
+        }
+
+        private void HorizontalMenuItem1_Click(object sender, EventArgs e)
+        {
+            VerticalMenuItem1_Click(sender, e);
+        }
+        private void HorizontalMenuItem2_Click(object sender, EventArgs e)
+        {
+            VerticalMenuItem2_Click(sender, e);
+        }
+        private void HorizontalMenuItem3_Click(object sender, EventArgs e)
+        {
+            VerticalMenuItem3_Click(sender, e);
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            
-            if (legend.Enabled = true)
+            //chartArea.AxisX.IntervalOffset += 10;
+            chartArea.AxisX2.IntervalOffset += 10;
+            //chartArea.AxisX2.IntervalOffset += 10;
+        }
+
+        private void VerticalMenuItem3_Click(object sender, EventArgs e)
+        {
+            if (legend.Enabled == true)
             {
                 legend.Enabled = false;
-                button1.Text = "Pokaż Legendę";
+                VerticalMenuItem3.Text = "Pokaż Legendę";
+                HorizontalMenuItem3.Checked = true;
             }
-                
+
             else
             {
                 legend.Enabled = true;
-                button1.Text = "Ukryj Legendę";
+                VerticalMenuItem3.Text = "Ukryj Legendę";
+                HorizontalMenuItem3.Checked = false;
             }
-                
-            
         }
+
+        
+
+        
+
+
     }
 }
